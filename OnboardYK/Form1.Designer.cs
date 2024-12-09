6395f8b925b7eca7eff41dd05b6b577f60eef4a2
Oscar Virot <oscar.virot@toriv.com>
2024-12-09 19:28:07 +0100

    Inital commit

---
+++ OnboardYK/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace OnboardYK
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            quitToolStripMenuItem = new ToolStripMenuItem();
+            selectYubikeyToolStripMenuItem = new ToolStripMenuItem();
+            onboardToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip2 = new MenuStrip();
+            panel1 = new Panel();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            label1 = new Label();
+            menuStrip2.SuspendLayout();
+            panel1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // quitToolStripMenuItem
+            // 
+            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            quitToolStripMenuItem.Size = new Size(80, 36);
+            quitToolStripMenuItem.Text = "Quit";
+            quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
+            // 
+            // selectYubikeyToolStripMenuItem
+            // 
+            selectYubikeyToolStripMenuItem.Name = "selectYubikeyToolStripMenuItem";
+            selectYubikeyToolStripMenuItem.Size = new Size(189, 36);
+            selectYubikeyToolStripMenuItem.Text = "Select Yubikey";
+            // 
+            // onboardToolStripMenuItem
+            // 
+            onboardToolStripMenuItem.Name = "onboardToolStripMenuItem";
+            onboardToolStripMenuItem.Size = new Size(128, 36);
+            onboardToolStripMenuItem.Text = "Onboard";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(99, 36);
+            aboutToolStripMenuItem.Text = "About";
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.ImageScalingSize = new Size(32, 32);
+            menuStrip2.Items.AddRange(new ToolStripItem[] { quitToolStripMenuItem, selectYubikeyToolStripMenuItem, onboardToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip2.Location = new Point(0, 0);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(800, 40);
+            menuStrip2.TabIndex = 1;
+            menuStrip2.Text = "menuStrip2";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 52);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 350);
+            panel1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(32, 32);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 408);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 42);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(237, 32);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(140, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(panel1);
+            Controls.Add(menuStrip2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private ToolStripMenuItem selectYubikeyToolStripMenuItem;
+        private ToolStripMenuItem onboardToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private MenuStrip menuStrip2;
+        private Panel panel1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Label label1;
+    }
+}
---
+++ OnboardYK/Form1.cs
@@ -0,0 +1,21 @@
+using System.Windows.Forms;
+
+namespace OnboardYK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
